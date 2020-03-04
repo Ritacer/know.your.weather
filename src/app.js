19f432d7bb156b5dd0a6a6151df04dd3263cbe75
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-03-04 23:43:07 +0000

    API integration

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
+let apiKey = "b8b2182b5c88a4be00e7803c6396e4a0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
